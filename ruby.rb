99d9233d7abd5e4922bad633be2c8364d836c8b2
LindsayHogg <lindsayhogg@gmail.com>
2015-07-13 14:48:51 -0400

    Sigh

--- ruby.rb
+++ ruby.rb
@@ -3,16 +3,18 @@ require 'nokogiri'
 require 'open-uri'
 
 get '/' do 
-	"hello from here."
-	end
+	url = "http://tinybuddha.com/category/blog/happiness-fun-blog/"
+	data = Nokogiri::HTML(open(url))
+	@content = data.css('#content')
+	erb :shows
+
+	# @content.each do |articles|
+	# puts data.at_css('#content').text
+	# end
+
+end
 
 
-# url = "http://tinybuddha.com/category/blog/happiness-fun-blog/"
 
-# data = Nokogiri::HTML(open(url))
 
-# content = data.css('#content')
 
-# content.each do |articles|
-# 	puts data.at_css('#content').text
-# end
