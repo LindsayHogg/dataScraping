cc05367495a558ef821fbcc47cdccb7f3d7d3276
LindsayHogg <lindsayhogg@gmail.com>
2015-07-13 17:14:39 -0400

    Yay content showing up!

--- ruby.rb
+++ ruby.rb
@@ -2,19 +2,19 @@ require 'sinatra'
 require 'nokogiri'
 require 'open-uri'
 
-get '/' do 
-	url = "http://tinybuddha.com/category/blog/happiness-fun-blog/"
-	data = Nokogiri::HTML(open(url))
-	@content = data.css('#content')
-	erb :shows
+# get '/' do 
+# 	url = "http://tinybuddha.com/category/blog/happiness-fun-blog/"
+# 	data = Nokogiri::HTML(open(url))
+# 	@content = data.css('#content')
+# 	erb :shows
+# end
 
+url = "http://tinybuddha.com/category/blog/happiness-fun-blog/"
+data = Nokogiri::HTML(open(url))
+content = data.css('#content')
 
+puts content
 
-end
-
-	# @content.each do |articles|
-	# puts data.at_css('#content').text
-	# end
 
 
 
